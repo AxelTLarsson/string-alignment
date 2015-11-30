84d47df0621fecb04a3fb8a2d53e883e31a884fe
AxelTLarsson <mail@axellarsson.nu>
2015-11-30 18:43:49 +0100

    Fix similarityScore so that it finds optimal score
    Previously I thought the input was the optimal alignment, but it
    turns out that similarityScore is also supposed to find that optimal
    alignment, or at least the score of it. The implementation now is
    the brute force one. Later I will make a memoized version that will
    hopefully work much faster.

--- src/Main.hs
+++ src/Main.hs
@@ -3,21 +3,55 @@ module Main where
 main :: IO ()
 main = return ()
 
-s1, t1, s2, t2 :: String
+s1, t1, s2, t2, s3, t3 :: String
 s1 = "HASKELL"
 t1 = "PASCA-L"
 s2 = "H-ASKELL"
 t2 = "-PASC-AL"
+s3 = "writers"
+t3 = "vintner"
 
 scoreMatch, scoreMismatch, scoreSpace :: Int
-scoreMatch = 1
-scoreMismatch = -1
-scoreSpace = -2
+scoreMatch = 0
+scoreMismatch = (-1)
+scoreSpace = (-1)
 
+-- return the optimal alignment of two strings
 similarityScore :: String -> String -> Int
+similarityScore [] _ = scoreSpace
+similarityScore _ [] = scoreSpace
+similarityScore x'@(x:xs) y'@(y:ys) = maxi case1 case2 case3 where
+    case1 = similarityScore xs ys + colScore x y    -- two non-space chars
+    case2 = similarityScore xs y' + colScore x '-'  -- non-space above, space below
+    case3 = similarityScore x' ys + colScore '-' y  -- space above, non-space below
+    maxi :: Int -> Int -> Int -> Int
+    maxi a b c = max (max a b) c
+
+
+colScore :: Char -> Char -> Int
+colScore _ '-' = scoreSpace
+colScore '-' _ = scoreSpace
+colScore x y
+    | x == y    = scoreMatch
+    | otherwise = scoreMismatch
+
+{- This was a misunderstanding; I thought here that similarityScore should
+take the optimal alignment as parameters and simply count the score
 similarityScore [] _ = 0
 similarityScore _ [] = 0
 similarityScore (s:ss) (t:ts)
-    | s == t    = scoreMatch + similarityScore ss ts
     | s == '-' || t == '-' = scoreSpace + similarityScore ss ts
+    | s == t    = scoreMatch + similarityScore ss ts
     | otherwise = scoreMismatch + similarityScore ss ts
+-}
+
+-- Given a list of pairs of two lists as the third parameter, cons
+-- the first parameter to the first element of each pair (i.e. the first list
+-- in the pairs), cons the second parameter to the second element of each pair (i.e. 
+-- the other list in the pairs)
+-- Take two heads and attach (cons) them at the beginning of each pair in the
+-- list of pairs of lists given as third parameter, e.g.
+-- > attachHeads '!' '?' [("hello", "can"), ("you", "hear")]
+-- > [("!hello", "?can"), ("!you", "?hear")]
+attachHeads :: a -> a -> [([a], [a])] -> [([a], [a])]
+attachHeads h1 h2 aList = [(h1:xs, h2:ys) | (xs, ys) <- aList]
