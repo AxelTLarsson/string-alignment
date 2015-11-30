0b0400527440e79d5d6cb968a5290f1fe8eb4a51
AxelTLarsson <mail@axellarsson.nu>
2015-11-30 17:28:02 +0100

    Add similarityScore function

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,23 @@
 module Main where
 
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = return ()
+
+s1, t1, s2, t2 :: String
+s1 = "HASKELL"
+t1 = "PASCA-L"
+s2 = "H-ASKELL"
+t2 = "-PASC-AL"
+
+scoreMatch, scoreMismatch, scoreSpace :: Int
+scoreMatch = 1
+scoreMismatch = -1
+scoreSpace = -2
+
+similarityScore :: String -> String -> Int
+similarityScore [] _ = 0
+similarityScore _ [] = 0
+similarityScore (s:ss) (t:ts)
+    | s == t    = scoreMatch + similarityScore ss ts
+    | s == '-' || t == '-' = scoreSpace + similarityScore ss ts
+    | otherwise = scoreMismatch + similarityScore ss ts
