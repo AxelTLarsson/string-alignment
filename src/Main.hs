5e7f7358cd3edeb0438c66f831db920efdbee493
AxelTLarsson <mail@axellarsson.nu>
2015-12-03 13:06:01 +0100

    Fix optAlignments function
    This is only the naive variant so far but it seems to be working
    correctly. A major bug was maximaBy score case1 ++ case2 ++ case3
    which should instead read maximaBy score $ case1 ++ case2 ++ case3

--- src/Main.hs
+++ src/Main.hs
@@ -3,13 +3,19 @@ module Main where
 main :: IO ()
 main = return ()
 
-s1, t1, s2, t2, s3, t3 :: String
+s1, t1, s2, t2, s3, t3, s4, t4, s5, t5, s6, t6 :: String
 s1 = "HASKELL"
 t1 = "PASCA-L"
 s2 = "H-ASKELL"
 t2 = "-PASC-AL"
 s3 = "writers"
 t3 = "vintner"
+s4 = "writ-ers"
+t4 = "vintner-"
+s5 = "wri-t-ers"
+t5 = "-vintner-"
+s6 = "wri-t-ers"
+t6 = "v-intner-"
 
 scoreMatch, scoreMismatch, scoreSpace :: Int
 scoreMatch = 0
@@ -24,8 +30,8 @@ similarityScore x'@(x:xs) y'@(y:ys) = max3 case1 case2 case3 where
     case1 = similarityScore xs ys + colScore (x,y)    -- two non-space chars
     case2 = similarityScore xs y' + colScore (x,'-')  -- non-space above, space below
     case3 = similarityScore x' ys + colScore ('-',y)  -- space above, non-space below
-    max3 :: Int -> Int -> Int -> Int
-    max3 a b c = max (max a b) c
+max3 :: Int -> Int -> Int -> Int
+max3 a b c = max (max a b) c
 
 
 colScore :: (Char, Char) -> Int
@@ -37,13 +43,14 @@ colScore (x, y)
 
 {- This was a misunderstanding; I thought here that similarityScore should
 take the optimal alignment as parameters and simply count the score
-similarityScore [] _ = 0
-similarityScore _ [] = 0
-similarityScore (s:ss) (t:ts)
-    | s == '-' || t == '-' = scoreSpace + similarityScore ss ts
-    | s == t    = scoreMatch + similarityScore ss ts
-    | otherwise = scoreMismatch + similarityScore ss ts
 -}
+score :: AlignmentType -> Int
+score ((s:ss), (t:ts))
+    | s == '-' || t == '-' = scoreSpace + score (ss, ts)
+    | s == t    = scoreMatch + score (ss, ts)
+    | otherwise = scoreMismatch + score (ss, ts)
+score (_, _) = 0
+
 
 -- Given a list of pairs of two lists as the third parameter, cons
 -- the first parameter to the first element of each pair (i.e. the first list
@@ -54,7 +61,7 @@ similarityScore (s:ss) (t:ts)
 -- > attachHeads '!' '?' [("hello", "can"), ("you", "hear")]
 -- > [("!hello", "?can"), ("!you", "?hear")]
 attachHeads :: a -> a -> [([a], [a])] -> [([a], [a])]
-attachHeads h1 h2 aList = [(h1:xs, h2:ys) | (xs, ys) <- aList]
+attachHeads h1 h2 aList = [(h1:xs,h2:ys) | (xs,ys) <- aList]
 
 
 maximaBy :: Ord b => (a -> b) -> [a] -> [a]
@@ -63,10 +70,17 @@ maximaBy valueFcn xs = filter (\x -> valueFcn x == maxVal) xs
 
 
 type AlignmentType = (String, String)
-{-
+
 optAlignments :: String -> String -> [AlignmentType]
-optAlignments x'@(x:xs) y'@(y:ys) = maximaBy colScore list
-    where list = [(x,y), (x,'-'), ('-',y)]
+optAlignments x'@(x:xs) y'@(y:ys) = maximaBy score $ case1 ++ case2 ++ case3
+    where case1 = attachHeads x y $ optAlignments xs ys
+          case2 = attachHeads x '-' $ optAlignments xs y'
+          case3 = attachHeads '-' y $ optAlignments x' ys
+optAlignments [] (y:ys) = attachHeads '-' y $ optAlignments [] ys
+optAlignments (x:xs) [] = attachHeads x '-' $ optAlignments xs []
+optAlignments [] [] = [([],[])]
 
-
--}
+a3, a4, a5 :: AlignmentType
+a3 = (s3, t3)
+a4 = (s4, t4)
+a5 = (s5, t5)
